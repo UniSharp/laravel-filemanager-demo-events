b23c53f227b331cf172787f3fb7b19a8fb14eb21
gwleuverink <willem@mediacode.nl>
2017-10-02 14:10:32 +0200

    finished delete eventlistener example

---
+++ app/Listeners/DeleteImageListener.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Listeners;
+
+use App\FilePath;
+use Unisharp\Laravelfilemanager\Events\ImageIsDeleting;
+
+class DeleteImageListener
+{
+    /**
+     * Handle the ImageIsDeleting event.
+     * If the image that is to be deleted exists inside the file_paths table,
+     * block the deletion of that image and show appropriate response.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(ImageIsDeleting $event)
+    {
+
+
+        // Get the public path for the file
+        $publicFilePath = str_replace(public_path(), "", $event->path());
+
+        // Search for usages of the file
+        $used = FilePath::where('path', $publicFilePath)->get();
+
+        if($used) {
+            // the image is in use, create a response message
+
+            $message = $this->formatMessage($used);
+
+            // Die with response message
+            die($message);
+        }
+    }
+
+    private function formatMessage($files)
+    {
+        $message = "<p>The file you are trying to delete is in use in the file_paths table with the following id's:</p>";
+        $message .= "<ul>";
+        foreach ($files as $file) {
+            $message .= "<li>$file->id</li>";
+        }
+        $message .= "</ul>";
+        $message .= "<p>Please remove those database references before you can delete the file.</p>";
+        return $message;
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,10 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
+use Unisharp\Laravelfilemanager\Events\ImageIsDeleting;
+use Unisharp\Laravelfilemanager\Events\ImageIsRenaming;
+use App\Listeners\DeleteImageListener;
+
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -15,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         'App\Events\Event' => [
             'App\Listeners\EventListener',
+        ],
+        ImageIsDeleting::class => [
+            DeleteImageListener::class,
         ],
     ];
 
